73d97f69ebcb27ccecc80b37cbc3ef1c5936db8c
Lucas Amarilla <amarilla482@gmail.com>
2021-11-23 08:58:56 -0400

    navbar_correction

--- views/about.php
+++ views/about.php
@@ -3,21 +3,17 @@
 
     <div class="about__container bd-grid">
         <div class="about__img">
-            <img src="assets/img/foto_ofcc.JPG" alt="">
+            <img src="assets/img/foto_ofcc.jpg" alt="">
         </div>
 
         <div>
             <h2 class="about__subtitle">Meu nome é Lucas Amarilla</h2>
-            <span class="about__profession">Desenvolvedor Mobile/Web - Editor de video</span>
-            <p class="about__text">Sou um jovem desenvolvedor que ama técnologia e sempre sonhou em trabalhar na area, em 2020 em meio a pandemia me formei em Técnico em informatia
-            no IFMS - Campus Ponta Porã, e no segundo semestre de 2021 ingressei em Engenharia de controle e automação na UCDB - Campo Grande.
+            <span class="about__profession">Desenvolvedor Mobile/Web - FullStack</span>
+            <p class="about__text">Sou um jovem desenvolvedor que ama técnologia e sempre sonhou em trabalhar na area, então decidi entrar em um curso desde cedo
+                dentro do meu curso técnico aprendi diversas técnologias como Java, MySql, infraestrutura de rede, etc porém a area que mais me interessou foi desenvolvimento web
+                
             </p>
 
-            <div class="about__social">
-                <a href="#" class="about__social-icon"><i class='bx bxl-linkedin' ></i></a>
-                <a href="#" class="about__social-icon"><i class='bx bxl-github' ></i></a>
-                <a href="#" class="about__social-icon"><i class='bx bxl-dribbble' ></i></a>
-            </div>
             <a href="./assets/cv.pdf" target="pdf-frame" class="skills__name" target="_blank">Download do CV</a>
         </div>
     </div>
--- views/navbar.php
+++ views/navbar.php
@@ -1,20 +1,20 @@
 <header class="l-header">
-    <nav class="nav bd-grid">
-      <center>
-        <div class="nav__menu" id="nav-menu">
-            <ul class="nav__list">
-                <li class="nav__item"><a href="#home" class="nav__link active">Inicio</a></li>
-                <li class="nav__item"><a href="#about" class="nav__link">Sobre mim</a></li>
-                <li class="nav__item"><a href="#education" class="nav__link">Educação</a></li>
-                <li class="nav__item"><a href="#skills" class="nav__link">Habilidades</a></li>
-                <li class="nav__item"><a href="#contact" class="nav__link">Contato</a></li>
-            </ul>
-        </div>
+  <nav class="nav bd-grid">
+    <center>
+      <div class="nav__menu" id="nav-menu">
+        <ul class="nav__list">
+          <li class="nav__item"><a href="#home" class="nav__link active">Inicio</a></li>
+          <li class="nav__item"><a href="#about" class="nav__link">Sobre mim</a></li>
+          <li class="nav__item"><a href="#education" class="nav__link">Educação</a></li>
+          <li class="nav__item"><a href="#skills" class="nav__link">Habilidades</a></li>
+          <li class="nav__item"><a href="#contact" class="nav__link">Contato</a></li>
+        </ul>
+      </div>
+    </center>
+    <div class="nav__toggle" id="nav-toggle">
+      <h1>...</h1>
+    </div>
 
-        <div class="nav__toggle" id="nav-toggle">
-            <i class='bx bx-menu'></i>
-        </div>
-      </center>
 
-    </nav>
+  </nav>
 </header>
